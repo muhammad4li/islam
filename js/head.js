24ff052a5975effaf17d9512265ccb8a667699ef
DevHedronYT <altair.d.ghost@gmail.com>
2021-12-20 11:12:13 +0200

    Fixing Some CSS Issues

--- js/head.js
+++ js/head.js
@@ -3,6 +3,6 @@ document.write("\n\
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>\n\
     <meta name='viewport' content='width=device-width, initial-scale=1'>\n\
     <link rel='icon' href='./res/pfp.png'>\n\
-    <link rel='stylesheet' type='text/css' media='screen' href='../css/main.css'></link>\n\
+    <link rel='stylesheet' type='text/css' media='screen' href='./css/main.css'></link>\n\
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'/>\n\
 ")
